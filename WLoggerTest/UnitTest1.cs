f2b389ec8c90a181034d767234e13d452b10b4eb
Neema <Neema.G.Nejad@usace.army.mil>
2019-09-24 15:52:24 -0700

    added a test for the WLogger class

---
+++ WLoggerTest/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace WLoggerTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
