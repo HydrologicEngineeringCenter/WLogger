77c650a2363bdc3c24b4db4e5bc4da5f4b764057
Neema <Neema.G.Nejad@usace.army.mil>
2019-09-25 16:32:07 -0700

    added some asserts to WLoggerTest

--- WLoggerTest/WLoggerTest.cs
+++ WLoggerTest/WLoggerTest.cs
@@ -16,14 +16,18 @@ namespace WLoggerTest
             Logger.Log(new ErrorMessage("Hey this isn't too bad", 1));
             Logger.Log(new TimeStampedMessage("Now the issue has been fixed!"));
             IMessageReceiver user1 = new MessageReceiver();
-            IMessageReceiver fuser1 = new FilteredMessageReceiver();
-            IMessageReceiver fuser2 = new FilteredMessageReceiver(typeof(IErrorMessage), 1);
+            IMessageReceiver fuser1 = new FilteredMessageReceiver(typeof(ITimeStampedMessage));
+            IMessageReceiver fuser2 = new FilteredMessageReceiver(typeof(IErrorMessage), 2);
             Logger.AddListener(user1);
             Logger.AddListener(fuser1);
             Logger.AddListener(fuser2);
             Logger.Flush();
 
+            Assert.AreEqual(5, user1.GetAllMessages().Count);
 
+            Assert.AreEqual(2, fuser1.GetAllMessages().Count);
+
+            Assert.AreEqual(1, fuser2.GetAllMessages().Count);
 
         }
     }
