e4274c3377179e92dcfd20e5d85006fb2607a7f5
Neema <Neema.G.Nejad@usace.army.mil>
2019-09-26 08:17:37 -0700

    added test for Logging messages and sending messages to listeners.

--- WLoggerTest/WLoggerTest.cs
+++ WLoggerTest/WLoggerTest.cs
@@ -10,11 +10,16 @@ namespace WLoggerTest
         public void CreateWLogger()
         {
             var Logger = WLogger.WLogger.getLogger();
-            Logger.Log(new BasicMessage("This is a regular message."));
-            Logger.Log(new TimeStampedMessage("This is the time I logged a message."));
-            Logger.Log(new ErrorMessage("Uh oh, this is pretty bad", 2));
-            Logger.Log(new ErrorMessage("Hey this isn't too bad", 1));
-            Logger.Log(new TimeStampedMessage("Now the issue has been fixed!"));
+            var a = "This is a regular message.";
+            var b = "This is the time I logged a message.";
+            var c = "Uh oh, this is pretty bad";
+            var d = "Hey this isn't too bad";
+            var e = "Now the issue has been fixed!";
+            Logger.Log(new BasicMessage(a));
+            Logger.Log(new TimeStampedMessage(b));
+            Logger.Log(new ErrorMessage(c, 2));
+            Logger.Log(new ErrorMessage(d, 1));
+            Logger.Log(new TimeStampedMessage(e));
             IMessageReceiver user1 = new MessageReceiver();
             IMessageReceiver fuser1 = new FilteredMessageReceiver(typeof(ITimeStampedMessage));
             IMessageReceiver fuser2 = new FilteredMessageReceiver(typeof(IErrorMessage), 2);
@@ -24,10 +29,19 @@ namespace WLoggerTest
             Logger.Flush();
 
             Assert.AreEqual(5, user1.GetAllMessages().Count);
+            Assert.AreEqual(a, user1.GetAllMessages()[0].GetMessage());
+            Assert.AreEqual(b, user1.GetAllMessages()[1].GetMessage().Substring(23));
+            Assert.AreEqual(c, user1.GetAllMessages()[2].GetMessage());
+            Assert.AreEqual(d, user1.GetAllMessages()[3].GetMessage());
+            Assert.AreEqual(e, user1.GetAllMessages()[4].GetMessage().Substring(23));
 
             Assert.AreEqual(2, fuser1.GetAllMessages().Count);
+            Assert.AreEqual(b, fuser1.GetAllMessages()[0].GetMessage().Substring(23));
+            Assert.AreEqual(e, fuser1.GetAllMessages()[1].GetMessage().Substring(23));
 
             Assert.AreEqual(1, fuser2.GetAllMessages().Count);
+            Assert.AreEqual(c, fuser2.GetAllMessages()[0].GetMessage());
+            Assert.AreEqual(2, ((IErrorMessage)fuser2.GetAllMessages()[0]).GetErrorLevel());
 
         }
     }
